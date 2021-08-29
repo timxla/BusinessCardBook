9b5d11f1253b70ee8e90d8ad3c6c2cb8f247d9f4
timxla <timtmdgus@icloud.com>
2021-08-29 16:40:30 +0900

    paging

--- v3_sqlAlchemy.py
+++ v3_sqlAlchemy.py
@@ -1,3 +1,5 @@
+# Namecard Manager with PostgreSQL and SQLAlchemy
+
 import logging
 import sys
 from typing import List
@@ -7,10 +9,11 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#Create the database
+# Create the engine
 engine = create_engine('postgresql+psycopg2://postgres:1234@localhost:5432/namecard')
 base = declarative_base()
 
+# Namecard class (SQLAlchemy)
 class Namecard(base):
     __tablename__ = 'book'
 
@@ -20,17 +23,21 @@ class Namecard(base):
     email = Column(String)
     phone_num = Column(String)
 
+# Sync metadata
 base.metadata.create_all(engine)
 
 
+# Datamanager class:
+# Create session, make a reciever instance (for input)
+# Query from session and save to self.cards beforehand (If table is large, query per action)
 class DataManager:
 
     def __init__(self):
         Session = sessionmaker(engine)
         self.session = Session()
         self.reciever = Reciever()
-        self.cards = self.session.query(Namecard)
     
+    # Add a card with data obtained from reciever
     def insertCard(self):
         new_entry = Namecard(name = self.reciever.recieveName(),
                             company = self.reciever.recieveCompanyName(),
@@ -39,27 +46,46 @@ class DataManager:
         self.session.add(new_entry)
         self.session.commit()
 
+    def queryAll(self):
+        return self.session.query(Namecard).all()
+
+    def queryByPage(self, offset, limit):
+        return self.session.query(Namecard).offset(offset).limit(limit).all()
+
+    def queryDBSize(self):
+        return self.session.query(Namecard).count()
+
+    # List all cards
     def listCards(self):
+        # Implememnt Paging
         data_buffer = []
-        for card in self.cards:
-            data_buffer.append([card.id, card.name, card.company, card.email, card.phone_num])
-        print("\n")
-        print(tabulate(data_buffer, headers=["ID", "name", "company", "email", "phone number"]))
+        number_of_pages = int(self.queryDBSize()/5 + 1)  
+        for i in range(number_of_pages):
+            page = self.queryByPage(i*5, 5)
+            for card in page:
+                data_buffer.append([card.id, card.name, card.company, card.email, card.phone_num])
+            print("\n")
+            print("Page " + str(i) + "\n")
+            print(tabulate(data_buffer, headers=["ID", "name", "company", "email", "phone number"]))
+            if i == number_of_pages - 1:
+                return
+            else:
+                print("See next page? \n")
+                if(self.reciever.userAnswer() == "Y"):
+                    data_buffer.clear()
+                    continue
+                else:
+                    data_buffer.clear()
+                    break
 
-    def selectCardID(self):
-        id = int(input("\nWhich row would you like to update? Enter ID :"))        
-        return id
-
-    def selectField(self):
-        field = str(input("\nEnter field that you would like to update?\n(name, company, email, phone_num) :"))
-        return field
-
+    # Delete card
     def deleteCard(self):
-        self.cards.filter(Namecard.id == self.selectCardID()).delete()
+        self.cards.filter(Namecard.id == self.reciever.selectCardID()).delete()
 
+    # Edit card
     def editCard(self):
-        x = self.cards.get(self.selectCardID())
-        field = self.selectField()
+        x = self.cards.get(self.reciever.selectCardID())
+        field = self.reciever.selectField()
         if (field == "name"):
             newdata = str(input("Enter new name : "))
             x.name = newdata
@@ -73,12 +99,15 @@ class DataManager:
             newdata = str(input("Enter new phone number : "))
             x.phone_num = newdata
 
+    # Close session, dispose engine and end program
     def terminate(self):
         self.session.close()
         engine.dispose()
         logging.info("program terminated.")
 
           
+# Reciever Class
+# Responisible for recieving all input and checking for exceptions
 class Reciever:
 
     def recieveCompanyName(self):
@@ -121,8 +150,26 @@ class Reciever:
                 continue
             else:
                 return phone 
-        
 
+    # Select card ID for editing or deleting
+    def selectCardID(self):
+        id = int(input("\nWhich row would you like to update? Enter ID :"))        
+        return id
+
+    # Select card Field for editing or deleting
+    def selectField(self):
+        field = str(input("\nEnter field that you would like to update?\n(name, company, email, phone_num) :"))
+        return field
+    
+    def userAnswer(self):
+        while True:
+            answer = str(input("Yes or No (Enter 'y' or 'n') : ")).upper()
+            if (answer == 'Y' or answer == 'N'):
+                return answer
+            else:
+                logging.warning("Try again!\n")
+                continue
+            
 def main():
 
     logging.basicConfig(
@@ -137,6 +184,7 @@ def main():
                     "   - Edit : remove all entries \n"
                     "   - Quit : Exit program \n")
     
+    # Create Datamanger instance
     dm = DataManager()
 
     while True:
@@ -144,8 +192,8 @@ def main():
         print("___________________________________\n")
         print(command_info)
         print("___________________________________")
+        # Recieve user command, upper() for disabling case-sensitivity
         command = str(input("")).upper()
-        # recieve user command
         if (command == "INSERT"):
             dm.insertCard()
             
